f83dd9722bc34f91590ef94a910e1a56af314e37
nponchon@xubuntu <nponchon@xubuntu.fr>
2026-03-26 17:07:58 +0100

    p1: switching to frrouting image instead of vanilla alpine

--- P1/docker_build.sh
+++ P1/docker_build.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-# Build and tag the images for GNS3 to use them
+# Pull FRR image from Quay
+docker pull quay.io/frrouting/frr:10.5.3
 
+docker tag quay.io/frrouting/frr:10.5.3 badass-frrouting
+
+# Build and tag the images for GNS3 to use them
 docker build ./host_nponchon/ -t "host_ponchon:latest"
 docker build ./router_nponchon/ -t "router_ponchon:latest"
 
